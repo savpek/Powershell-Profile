6ec6c7ef9301c7c1f67535324dd87c531ef0f19a
savpek <savolainen.pekka@gmail.com>
2013-06-10 20:35:30 +0300

    Now Open/Close notepads works with pipes.

--- modules/savpek/savpek.psm1
+++ modules/savpek/savpek.psm1
@@ -17,39 +17,15 @@ Function New-Shell
     start-process powershell.exe -argument "-nologo -noprofile -executionpolicy bypass -command $command $command2 $command3; read-host 'Exit'"	
 }
 
-Function Get-ToNotepad {
-    Param(
-	    [Parameter(Mandatory=$True, position=1)]
-	    [string]$FilePattern,
-	     
-	    [Parameter(Mandatory=$True, position=2)]
-	    [string]$Match
-    )
-
-    $files = dir $FilePattern -Recurse | Select-String "$Match";
-    $uniqueFiles = $files | Select-Object -Unique Path;
-
-    "All matches:";
-    foreach($file in $files) {
-        $file;
-    }
-
-    $opened = 0;
-    foreach($file in $uniqueFiles)
-    {
-        if($opened -eq 20) { 
-            Write-Error "Limit is 20 open notepads by single command.";
-            return;    
-        }
-        
-        $command = 'Notepad2 /m "=" "' + $file.Path + '"';
+Function Open-Notepads {
+    PROCESS {
+        $command = 'Notepad2 /g "' + $_.LineNumber + ',0" "' + $_.Path + '"';
+        $command;
         Invoke-Expression $command;
-        
-        $opened += 1;
     }
 }
 
-Function Stop-AllNotepads {
+Function Close-Notepads {
     Stop-Process -Name *Notepad2*;
 }
 
