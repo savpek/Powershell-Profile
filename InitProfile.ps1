8eb34fdad06ccf5d4ad1c419dfd4f2588cf5086a
labday <labday@lab.org>
2013-08-23 10:27:56 +0300

    Fixed custom path logig.

--- InitProfile.ps1
+++ InitProfile.ps1
@@ -13,7 +13,7 @@ $customPaths | Add-Member -type NoteProperty -Name Root -Value $root
 
 foreach($moduleFolder in (Get-ChildItem -Directory "$root\modules\")) 
 {
-    $customPaths | Add-Member -type NoteProperty -Name $moduleFolder.Name -Value $moduleFolder
+    $customPaths | Add-Member -type NoteProperty -Name $moduleFolder.Name -Value $moduleFolder.FullName
 }
 
 $env:path += ";$root\bin\Notepad2"
