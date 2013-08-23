3f65f97c7b2847ef1cfc4d0bbcd4e61cfcc39025
savpek <savolainen.pekka@gmail.com>
2013-08-23 20:05:21 +0300

    Fixed installation script to install poshgit correctly.

--- Install.ps1
+++ Install.ps1
@@ -1,3 +1,5 @@
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 
+Invoke-Expression "$scriptPath\modules\posh-git\install.ps1"
+
 Add-Content $profile ". '$scriptPath\InitProfile.ps1'"
