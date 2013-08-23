a931413d6a3c6f614b7bc2c31e8b68fcbbad80d7
labday <labday@lab.org>
2013-08-23 16:10:01 +0300

    Added functionality to find orphan test projects. (No matching project by name exist.)

--- modules/Custom/VisualStudioExtensions.ps1
+++ modules/Custom/VisualStudioExtensions.ps1
@@ -7,3 +7,38 @@ function Find-NotMatchingNamespace ()
 {
     Get-ChildItem -Recurse *.csproj | Select-Xml -XPath "//ns:RootNamespace" -Namespace @{ns="http://schemas.microsoft.com/developer/msbuild/2003"} | Where { $_.Path -notlike "*" + $_.Node.'#text' + "*" } | foreach { "Project: " + $_.Path + " and name: " + $_.Node.'#text' + " Does not match!" }
 }
+
+function Find-OrphanTestProjects () 
+{
+    $projects = Get-ChildItem -Recurse *.csproj
+
+    $nonTestProjects = $projects -notlike "*.*tests*.csproj"
+    $testProjects = $projects -like "*.*tests*.csproj"
+
+    foreach ($testProject in $testProjects) {
+        $tokens = $testProject -split "\\"
+        $testProjectPlainName = $tokens[-1] -replace "\.[a-z]*tests[a-z]*?.*\.csproj",""
+
+        $matching = $nonTestProjects -like "*\$testProjectPlainName.csproj"
+
+        if($matching.Length -eq 0) 
+        {
+            ""
+            Write-Host "Cannot find matching project for: $testProject" -ForegroundColor Yellow
+        }
+
+        if($matching.Length -gt 1) 
+        {
+            ""
+            Write-Host "Multiple matching project for: $testProject, Matching: $matching" -ForegroundColor Yellow
+        }
+    }
+}
+
+Find-OrphanTestProjects
+
+function Repair-VisualStudioStructure() 
+{
+    "Clearing .suo files..."
+    Get-ChildItem -Recurse *.suo | Remove-Item -Force
+}
