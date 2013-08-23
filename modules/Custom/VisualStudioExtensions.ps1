b07ebb03c7f7730a8ffd355f7e1213d3d15a9308
labday <labday@lab.org>
2013-08-23 13:02:43 +0300

    Tools to find not matching assemblies against namespace and project name.

---
+++ modules/Custom/VisualStudioExtensions.ps1
@@ -0,0 +1,9 @@
+function Find-NotMatchingAssemblyName () 
+{
+    Get-ChildItem -Recurse *.csproj | Select-Xml -XPath "//ns:AssemblyName" -Namespace @{ns="http://schemas.microsoft.com/developer/msbuild/2003"} | Where { $_.Path -notlike "*" + $_.Node.'#text' + "*" } | foreach { "Project: " + $_.Path + " and name: " + $_.Node.'#text' + " Does not match!" }
+}
+
+function Find-NotMatchingNamespace () 
+{
+    Get-ChildItem -Recurse *.csproj | Select-Xml -XPath "//ns:RootNamespace" -Namespace @{ns="http://schemas.microsoft.com/developer/msbuild/2003"} | Where { $_.Path -notlike "*" + $_.Node.'#text' + "*" } | foreach { "Project: " + $_.Path + " and name: " + $_.Node.'#text' + " Does not match!" }
+}
