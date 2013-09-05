947a5ac7780d5b51d73b573134d340a10586bad8
labday <labday@lab.org>
2013-09-05 19:12:45 +0300

    Repair and orphan project cmdlet updates.

--- modules/Custom/Repair-VisualStudioSolutions.ps1
+++ modules/Custom/Repair-VisualStudioSolutions.ps1
@@ -1,44 +1,22 @@
-function Find-NotMatchingAssemblyName () 
-{
-    Get-ChildItem -Recurse *.csproj | Select-Xml -XPath "//ns:AssemblyName" -Namespace @{ns="http://schemas.microsoft.com/developer/msbuild/2003"} | Where { $_.Path -notlike "*" + $_.Node.'#text' + "*" } | foreach { "Project: " + $_.Path + " and name: " + $_.Node.'#text' + " Does not match!" }
-}
-
-function Find-NotMatchingNamespace () 
-{
-    Get-ChildItem -Recurse *.csproj | Select-Xml -XPath "//ns:RootNamespace" -Namespace @{ns="http://schemas.microsoft.com/developer/msbuild/2003"} | Where { $_.Path -notlike "*" + $_.Node.'#text' + "*" } | foreach { "Project: " + $_.Path + " and name: " + $_.Node.'#text' + " Does not match!" }
-}
-
-function Find-OrphanTestProjects () 
-{
-    $projects = Get-ChildItem -Recurse *.csproj
-
-    $nonTestProjects = $projects -notlike "*.*tests*.csproj"
-    $testProjects = $projects -like "*.*tests*.csproj"
-
-    foreach ($testProject in $testProjects) {
-        $tokens = $testProject -split "\\"
-        $testProjectPlainName = $tokens[-1] -replace "\.[a-z]*tests[a-z]*?.*\.csproj",""
-
-        $matching = $nonTestProjects -like "*\$testProjectPlainName.csproj"
-
-        if($matching.Length -eq 0) 
-        {
-            ""
-            Write-Host "Cannot find matching project for: $testProject" -ForegroundColor Yellow
-        }
-
-        if($matching.Length -gt 1) 
-        {
-            ""
-            Write-Host "Multiple matching project for: $testProject, Matching: $matching" -ForegroundColor Yellow
-        }
-    }
-}
-
-Find-OrphanTestProjects
-
-function Repair-VisualStudioStructure() 
+function Repair-VisualStudioStructure([Switch] $tfs) 
 {
     "Clearing .suo files..."
     Get-ChildItem -Recurse *.suo | Remove-Item -Force
+
+    "Removing solution control crap from solution files."
+    $solutions = Get-ChildItem -Recurse *.sln
+
+    foreach($solution in $solutions) {
+        $contents = [System.IO.File]::ReadAllText($solution.Fullname)
+        $contents = $contents -replace "(?s)GlobalSection\(TeamFoundationVersionControl\).*?EndGlobalSection", ""
+
+        $path = $solution.FullName
+
+        if($tfs) {
+            tf.exe lock /lock:none "$path"
+            tf checkout "$path"
+        }
+
+        Set-Content -Value $contents -Path $path
+    }
 }
