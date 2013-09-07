3d34fe92f4bc72b247f11539cb39a1c8a8d92778
savpek <savolainen.pekka@gmail.com>
2013-09-07 18:06:54 +0300

    Removed unnecessary stuff from InitProfile and notepad2 (comes from chocolatey now.)

--- InitProfile.ps1
+++ InitProfile.ps1
@@ -4,16 +4,8 @@
 
 $env:PSModulePath += ";$root\modules\"
 
-Import-Module PsGet
 Import-Module Custom
 Import-Module CSharp
-
-try {
-    Import-Module Posh-Git
-}
-catch {
-    Write-Warning "Cannot load Posh-Git module, is it installed?"
-}
 
 $customPaths = New-Object PSObject
 $customPaths | Add-Member -type NoteProperty -Name Root -Value $root
@@ -23,5 +15,4 @@ foreach($moduleFolder in (Get-ChildItem -Directory "$root\modules\"))
     $customPaths | Add-Member -type NoteProperty -Name $moduleFolder.Name -Value $moduleFolder.FullName
 }
 
-$env:path += ";$root\bin\Notepad2"
 $env:GIT_EDITOR = "Notepad2.exe"
