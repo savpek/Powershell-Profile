92ba5224480527ab35acfb6e6c991d4385f5e8bb
Savpek <savolainen.pekka@gmail.com>
2014-03-28 15:36:43 +0200

    Added VirtualBox module.

---
+++ modules/VirtualBox/Get-Vm.ps1
@@ -0,0 +1,3 @@
+function Get-VM {
+    VBoxManage list vms
+}
---
+++ modules/VirtualBox/VirtualBox.psm1
@@ -0,0 +1,19 @@
+﻿Set-StrictMode -Version Latest
+
+$psScriptRoot = Split-Path -parent $PSCommandPath
+
+if(-not (Get-Command -CommandType Application | where { $_.name -eq 'VBoxManage.exe' })) {
+    throw "Cannot import VirtualBox module, reason: cannot find 'VBoxManage.exe' from path."
+}
+
+Push-Location $psScriptRoot
+
+$files = Get-ChildItem *.ps1
+foreach($file in $files) 
+{
+. $file
+}
+
+Pop-Location
+
+Set-StrictMode -Off
