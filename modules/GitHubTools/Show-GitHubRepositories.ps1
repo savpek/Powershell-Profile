f252fc83fda3d1a519b89f0b82111a4e4f2a70c6
Savpek <savolainen.pekka@gmail.com>
2014-04-21 12:12:42 +0300

    Improved git hub tools to support search directly from cmdlet.

--- modules/GitHubTools/Show-GitHubRepositories.ps1
+++ modules/GitHubTools/Show-GitHubRepositories.ps1
@@ -1,11 +1,13 @@
 function Show-GitHubRepositories {
-    $response = Invoke-WebRequest -Uri "https://api.github.com/users/savpek/repos" -Method Get
+    [CmdLetBinding()]
+    Param($Like = "")
+    $response = Invoke-WebRequest -Uri "https://api.github.com/users/savpek/repos?per_page=100" -Method Get
 
     if($response.StatusCode -ne "200") {
         throw "Invalid response code."
     }
 
-    $uris = $response.Content | ConvertFrom-Json | foreach { $_.html_url }
+    $uris = $response.Content | ConvertFrom-Json | foreach { $_.html_url } | where { $_ -like "*$Like*" }
 
     return $uris
 }
