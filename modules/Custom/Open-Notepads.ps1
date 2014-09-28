5302c2af5e938236a34b47c9fe8d91023aaa9353
Savpek <savolainen.pekka@gmail.com>
2014-09-28 11:05:41 +0300

    Fixed open notepads cmdlet.

--- modules/Custom/Open-Notepads.ps1
+++ modules/Custom/Open-Notepads.ps1
@@ -4,13 +4,21 @@ Function Open-Notepads {
     [CmdLetBinding()]
     Param(
         [Alias("FullName")]
-        [parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]$Path
+        [parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]$Path,
+        [parameter(ValueFromPipelineByPropertyName=$true)]$Pattern
     )
 
+    Begin {
+        $opened = @()
+    }
+
     PROCESS {
-        $command = 'Notepad2 /g "' + $_.LineNumber + ',0" "' + $Path + '"';
+        if($opened -contains $Path) {            return
+        }
+        $command = "Notepad /m '$pattern' '$Path'";
         $command;
         Invoke-Expression $command;
+        $opened += $Path
     }
 }
 
